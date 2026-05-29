ba1ec1f4a35aea1edf65b4d31ef7a89eb7efbe8c
Seid Arnaut <tvojemail@gmail.com>
2026-05-29 12:50:54 +0200

    fix pdf parser using pypdf

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,12 +1,13 @@
-import pdfplumber
+from pypdf import PdfReader
 
 def extract_text_from_pdf(file_path: str) -> str:
     text = ""
 
-    with pdfplumber.open(file_path) as pdf:
-        for page in pdf.pages:
-            page_text = page.extract_text()
-            if page_text:
-                text += page_text + "\n"
+    reader = PdfReader(file_path)
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text + "\n"
 
     return text
